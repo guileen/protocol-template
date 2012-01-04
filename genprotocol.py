dbb1b81f91d50b77fc39a3f5f5d88e32c7034535
Lin Gui <guileen@gmail.com>
2012-01-04 17:20:46 +0800

    add node template

--- genprotocol.py
+++ genprotocol.py
@@ -3,10 +3,35 @@
 import ConfigParser
 import json
 import os
+import re
 from jinja2 import Template
 
+# foo.rect = int8[4]
+# foo.members = string[]
+types = ('int8', 'int16', 'int32', 'uint8', 'uint16', 'uint32', 'utf8', 'ascii', 'date', 'array', 'reference')
+typeSizes = (1, 2, 4, 1, 2, 4, 0, 0, 4, 0, 0)
+
+def getRef(typeName, userTypes):
+    if typeName.startswith('$'):
+        return types.index('reference') | (userTypes.index(typeName[1:]) << 8)
+    else:
+        return types.index(typeName)
+
+# TODO: uint16[][][]
+def stripTypes(typeName):
+    match = re.match(r'([\d\w]+)\[\s*(\d*)\s*\]', typeName)
+    if match:
+    # iSArray
+        length = match.group(2) or 0
+        return (getRef(match.group(1)), True, length)
+    # else:
+    # is Reference
+    else:
+    # is Primite
+        return (getRef(typeName), False, 0)
+
 def genFields(config, section):
-    return [(option, config.get(section, option)) for option in sorted(config.options(section))]
+    return [(option, stripTypes(config.get(section, option))) for option in sorted(config.options(section))]
 
 def genProtocol(input_file, options):
     config = ConfigParser.RawConfigParser()
---
+++ templates/protocol.template.js
@@ -0,0 +1,103 @@
+var Types = {{ type_json }}
+var TypeSizes = {{ type_sizes_json }}
+
+{% for type in types -%}
+var TYPE_{{ type }} = {{ loop.index0 }};
+{% endfor %}
+
+function Message(){
+}
+
+var protocols = {{ protocols_json }}
+
+protocols.forEach(function(protocol, index){
+    // message.send_login
+    var protocol_name = protocol[0];
+    var protocol_sym = protocol_name.toLowerCase().replace('-', '_');
+    var fields = protocol[1];
+
+    Message.prototype['send_' + protocol_sym] = function(msg) {
+      // send Protocol index
+      this._queue.push([Buffer.prototype.writeUInt16BE, [index]]);
+
+      // send fields
+      for(var i=0;i<fields.length;i++) {
+        var field = fields[i];
+        var field_name = field[0];
+        var field_type = field[1];
+        this._prepare(field_type, msg[field_name]);
+      }
+    }
+});
+
+Message.prototype.write = function() {
+  var buffer = new Buffer(this.size);
+  var q = this._queue;
+  for(var i = 0; i < q.length; i++) {
+    var v = q[i];
+    v[0].apply(buffer, v[1]);
+  }
+  return buffer;
+}
+
+// TODO: string[][], $ref[]
+Message.prototype._prepare = function(field_type, value) {
+  var type = field_type[0] & 0xFF;
+  var isArray = field_type[1];
+  if(isArray) {
+    // assert(Array.isArray(value))
+    var arrLen = field_type[2];
+    if(!arrLen) {
+      arrLen = value.length;
+      this._pendingLen(arrLen);
+    }
+    for(var i=0; i< arrLen; i++) {
+      var v = value[i] || '';
+      this._pending(type, v);
+    }
+  } else {
+    this._pending(type, v);
+  }
+}
+
+Message.prototype._pendingLen(len) {
+  {% if max255 -%}
+  this._queue.push([Buffer.prototype.writeUInt8, [len]]);
+  this.size += 1;
+  {% else %}
+  this._queue.push([Buffer.prototype.writeUInt16BE, [len]]);
+  this.size += 2;
+  {% endif %}
+}
+
+Message.prototype._pending = function(type, value) {
+  switch(type) {
+  case {{ types.index('uint8') }}:
+    this._queue.push([Buffer.prototype.writeUInt8, [value]]);
+    this.size += 1;
+    return;
+  case {{ types.index('uint16') }}:
+    this._queue.push([Buffer.prototype.writeUInt16BE, [value]]);
+    this.size += 2;
+    return;
+  case {{ types.index('uint32') }}:
+    this._queue.push([Buffer.prototype.writeUInt32BE, [value]]);
+    this.size += 4;
+    return;
+  case {{ types.index('uint64') }}:
+    //TODO
+    return;
+  case {{ types.index('utf8') }}:
+    var len = Buffer.byteLength(value, 'utf8');
+    this._pendingLen(len);
+    this._queue.push([Buffer.prototype.write, [value, 0, len, 'utf8']]);
+    this.size += len;
+    return;
+  case {{ types.index('ascii') }}:
+    var len = Buffer.byteLength(value, 'ascii');
+    this._pendingLen(len);
+    this._queue.push([Buffer.prototype.write, [value, 0, len, 'ascii']]);
+    this.size += len;
+    return;
+  }
+}
